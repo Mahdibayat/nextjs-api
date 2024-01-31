3145629ea6a7b784f1d01f101c3b2c6d4b5221a5
mahdibayat <mahdibayat72@hotmail.com>
2024-01-31 21:15:15 +0330

    first api

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected To Database'))
+db.once('open', () => console.log('Connected To Database'));
+
+app.use(express.json());
+
+const subscribersRoutes = require('./routes/subscribers.js')
+app.use('/subscribers', subscribersRoutes)
 
 
 app.listen(3000, ()=>console.log("SERVER START"));
